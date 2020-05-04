9242718c0c0eae1e432a7b2e4119faaa2577f2fb
Hamed <zargaripour@users.noreply.github.com>
2020-05-04 20:10:04 +0430

    ux: style of pure profit

--- realPNL.js
+++ realPNL.js
@@ -39,61 +39,55 @@ min-width: 192px;
 font-size: 0.9em;
 }
 
-#realPNL .real-data {
+#realPNL .real-data span:nth-child(1) {
+font-weight: normal;
+color: #888;
+}
+
+#realPNL .real-data span:nth-child(2) {
 font-weight: bold;
 }
 `);
 
 
 $.noConflict();
-jQuery( document ).ready(function($) {
+jQuery(document).ready(function ($) {
     var url = window.location.pathname;
     var box =
         '<div id="realPNL">' +
-        '<div class="real-border">'+
-        '<div class="real-title">Real PNL (Pure Profit)</div>'+
-        '<div class="real-data">...</div>' +
-        '</div>' +
+            '<div class="real-border">' +
+                '<div class="real-title">Real PNL (Pure Profit)</div>' +
+                '<div class="real-data"><span>...</span> <span>(...)</span></div>' +
+            '</div>' +
         '</div>';
 
-    if(url.match('\/futures\/') !== null )
-    {
-        setInterval(function()
-        {
-            if($(".position-tab").length !== 0)
-            {
-                if($("#realPNL").length !== 0)
-                {
-                    if($('.trade-history .position-tab .ReactVirtualized__Grid__innerScrollContainer').children().length > 13)
-                    {
+    if (url.match('\/futures\/') !== null) {
+        setInterval(function () {
+            if ($(".position-tab").length !== 0) {
+                if ($("#realPNL").length !== 0) {
+                    if ($('.trade-history .position-tab .ReactVirtualized__Grid__innerScrollContainer').children().length > 13) {
                         var position = $('.trade-history .position-tab .ReactVirtualized__Grid__innerScrollContainer > div');
                         var positionSize = parseFloat(position.filter(':nth-child(12)').text().replace(/,/g, ''));
                         var positionPrice = parseFloat(position.filter(':nth-child(13)').text().replace(/,/g, ''));
                         var currentPrice = parseFloat($('.header-pc > div:nth-child(2) > div:nth-child(1) > div:nth-child(3)').text().replace(/,/g, ''));
                         var realPNL_sum = ((currentPrice - positionPrice) * positionSize).toFixed(2);
-                        var realPNL_pure = (realPNL_sum - (Math.abs((currentPrice+positionPrice)*positionSize) * 0.0004)).toFixed(2);
-                        $("#realPNL .real-data").text(realPNL_sum + ' (' + realPNL_pure + ')');
+                        var realPNL_pure = (realPNL_sum - (Math.abs((currentPrice + positionPrice) * positionSize) * 0.0004)).toFixed(2);
+                        $("#realPNL .real-data span:nth-child(1)").text(realPNL_sum);
+                        $("#realPNL .real-data span:nth-child(2)").text('(' + realPNL_pure + ')');
+                    } else {
+                        $("#realPNL .real-data span:nth-child(1)").text('...');
+                        $("#realPNL .real-data span:nth-child(2)").text('(...)');
                     }
-                    else
-                    {
-                        $("#realPNL .real-data").text('...');
-                    }
-                }
-                else
-                {
+                } else {
                     $('.trade-history .position-tab .ReactVirtualized__Grid').append(box);
                 }
             }
         }, 1500);
 
-        console.log('realPNL: ' , 'yeah! some dudes are working on your real PNL, don\'t disturb them.;)');
-    }
-    else if (url.match('\/futuresng\/') !== null)
-    {
-        console.log('realPNL: ' , 'Oh crap! this is an new version. I will work on it, sure when it be stable.');
-    }
-    else
-    {
-        console.log('realPNL: ' , 'Oops, realPNL doesn\'t work correctly. create an issue on github please.');
+        console.log('realPNL: ', 'yeah! some dudes are working on your real PNL, don\'t disturb them.;)');
+    } else if (url.match('\/futuresng\/') !== null) {
+        console.log('realPNL: ', 'Oh crap! this is an new version. I will work on it, sure when it be stable.');
+    } else {
+        console.log('realPNL: ', 'Oops, realPNL doesn\'t work correctly. create an issue on github please.');
     }
 });
