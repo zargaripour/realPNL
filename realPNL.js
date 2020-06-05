6d780067c6aa26662ef85b449044ed88624f51fe
Hamed <zargaripour@users.noreply.github.com>
2020-06-06 04:11:28 +0430

    now Supports new website of "futures"

--- realPNL.js
+++ realPNL.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         realPNL
 // @description  real PNL based on "Binance Futures price" itself
-// @version      1.1
+// @version      1.2
 // @author       Hamed Zargaripour
 // @namespace    https://github.com/zargaripour/realPNL
 // @updateURL    https://raw.githubusercontent.com/zargaripour/realPNL/master/realPNL.js
@@ -33,6 +33,7 @@ background-color: #1b2027;
 padding: 8px 16px;
 margin: 2px;
 min-width: 192px;
+color: #fff;
 }
 
 #realPNL .real-title {
@@ -55,10 +56,10 @@ jQuery(document).ready(function ($) {
     var url = window.location.pathname;
     var box =
         '<div id="realPNL">' +
-            '<div class="real-border">' +
-                '<div class="real-title">Real PNL (Pure Profit)</div>' +
-                '<div class="real-data"><span>...</span> <span>(...)</span></div>' +
-            '</div>' +
+        '<div class="real-border">' +
+        '<div class="real-title">Real PNL (<b>Pure Profit<b>)</div>' +
+        '<div class="real-data"><span>...</span> <span>(...)</span></div>' +
+        '</div>' +
         '</div>';
 
     if (url.match('\/futures\/') !== null) {
@@ -83,10 +84,30 @@ jQuery(document).ready(function ($) {
                 }
             }
         }, 1500);
-
-        console.log('realPNL: ', 'yeah! some dudes are working on your real PNL, don\'t disturb them.;)');
     } else if (url.match('\/futuresng\/') !== null) {
-        console.log('realPNL: ', 'Oh crap! this is an new version. I will work on it, sure when it be stable.');
+        setInterval(function () {
+            var position = $(".css-4q7727 .list-grid .css-13rzmoo");
+            if (position.length !== 0)
+            {
+                if ($("#realPNL").length !== 0) {
+                    position = position.parent();
+                    if (position.children().length > 5) {
+                        var positionSize = parseFloat(position.children().filter(':nth-child(2)').text().replace(/,/g, ''));
+                        var positionPrice = parseFloat(position.children().filter(':nth-child(3)').text().replace(/,/g, ''));
+                        var currentPrice = parseFloat($('.css-xz4nxa .css-vurnku').text().replace(/,/g, ''));
+                        var realPNL_sum = ((currentPrice - positionPrice) * positionSize).toFixed(2);
+                        var realPNL_pure = (realPNL_sum - (Math.abs((currentPrice + positionPrice) * positionSize) * 0.0004)).toFixed(2);
+                        $("#realPNL .real-data span:nth-child(1)").text(realPNL_sum);
+                        $("#realPNL .real-data span:nth-child(2)").text('(' + realPNL_pure + ')');
+                    } else {
+                        $("#realPNL .real-data span:nth-child(1)").text('...');
+                        $("#realPNL .real-data span:nth-child(2)").text('(...)');
+                    }
+                } else {
+                    $("div").parent(".css-4q7727 .list-grid").append(box);
+                }
+            }
+        }, 1500);
     } else {
         console.log('realPNL: ', 'Oops, realPNL doesn\'t work correctly. create an issue on github please.');
     }
