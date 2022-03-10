d4a3daa430cba7e56f456c9f968c83a1903e289a
Hamed <zargaripour@users.noreply.github.com>
2022-03-11 03:18:30 +0330

    re-sync with last update
    - selectors modified.
    - Script has been reformatted.
    - UserScript header modified.

--- realPNL.js
+++ realPNL.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         realPNL
 // @description  real PNL based on "Binance Futures price" itself
-// @version      3.0
+// @version      3.1
 // @author       Hamed Zargaripour
 // @namespace    https://github.com/zargaripour/realPNL
-// @updateURL    https://raw.githubusercontent.com/zargaripour/realPNL/master/realPNL.js
 // @icon         https://bin.bnbstatic.com/static/images/common/favicon.ico
 // @match        https://www.binance.com/*/futures/*
+// @downloadURL  https://raw.githubusercontent.com/zargaripour/realPNL/master/realPNL.js
+// @updateURL    https://raw.githubusercontent.com/zargaripour/realPNL/master/realPNL.js
 // @match        https://www.binance.com/*/futuresng/*
 // @match        https://testnet.binancefuture.com/*/futures/*
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
@@ -54,73 +55,82 @@ color: rgb(14, 203, 129);
 
 $.noConflict();
 jQuery(document).ready(function ($) {
-    var url = window.location.pathname;
-    var hrf = window.location.href;
+    var url = window.location.pathname
+    var hrf = window.location.href
     var box =
         '<div id="realPNL">' +
-            '<div class="real-border">' +
-                '<div class="real-title"><b class="longside">Long Side<b></div>' +
-                '<div class="real-data longdata"><b class="longside">...<b></div>' +
-            '</div>' +
-            '<div class="real-border">' +
-                '<div class="real-title"><b class="shortside">Short Side<b></div>' +
-                '<div class="real-data shortdata"><b class="shortside">...<b></div>' +
-            '</div>' +
-        '</div>';
+        '<div class="real-border">' +
+        '<div class="real-title"><b class="longside">Long Side<b></div>' +
+        '<div class="real-data longdata"><b class="longside">...<b></div>' +
+        '</div>' +
+        '<div class="real-border">' +
+        '<div class="real-title"><b class="shortside">Short Side<b></div>' +
+        '<div class="real-data shortdata"><b class="shortside">...<b></div>' +
+        '</div>' +
+        '</div>'
 
-    if (url.match('\/futures\/') !== null || url.match('\/futuresng\/') !== null) {
-        setInterval(function () {
-            var position = $(".list-item-container").parent();
-            if (position.children().length !== 0) {
+    if (url.match('\/futures\/') !== null || url.match('\/futuresng\/') !== null)
+    {
+        setInterval(function ()
+                    {
+            var position = $('.list-item-container').parent()
 
-                if ($("#realPNL").length !== 0) {
-                    var long = false;
-                    var short = false;
-                    var item = null;
+            if (position.children().length !== 0)
+            {
+
+                if ($('#realPNL').length !== 0)
+                {
+                    var long = false
+                    var short = false
                     var positionSize = null
-                    var positionPrice = null;
-                    var currentPrice = null;
-                    var realPNL_sum = null;
-                    var realPNL_pure = null;
+                    var positionPrice = null
+                    var currentPrice = null
+                    var realPNL_sum = null
+                    var realPNL_pure = null
 
-                    var symbol = $("h1").first().text().replace(/\s/g,'');
+                    var symbol = $('h1').first().text().replace(/\s/g, '')
 
-                    position.children().each(function () {
-                        if($(this).find('.symbol .pair').text() == symbol)
+                    position.children().each(function ()
+                                             {
+                        if ($(this).find('.symbol .pair').text() === symbol)
                         {
-                            positionSize = parseFloat($(this).find('.size').text().replace(/,/g, ''));
-                            positionPrice = parseFloat($(this).find('.entryPrice').text().replace(/,/g, ''));
-                            currentPrice = parseFloat($(this).find('.markPrice').text().replace(/,/g, ''));
-                            realPNL_sum = ((currentPrice - positionPrice) * positionSize).toFixed(2);
-                            realPNL_pure = (realPNL_sum - (Math.abs((currentPrice + positionPrice) * positionSize) * 0.0004)).toFixed(2);
+                            positionSize = parseFloat($(this).find('.size').text().replace(/,/g, ''))
+                            positionPrice = parseFloat($(this).find('.entryPrice').text().replace(/,/g, ''))
+                            currentPrice = parseFloat($(this).find('.markPrice').text().replace(/,/g, ''))
+                            realPNL_sum = ((currentPrice - positionPrice) * positionSize).toFixed(2)
+                            realPNL_pure = (realPNL_sum - (Math.abs((currentPrice + positionPrice) * positionSize) * 0.0004)).toFixed(2)
 
-                            if($(this).find('.size-sell').length > 0)
+                            if ($(this).find('.size-sell').length > 0)
                             {
-                                $("#realPNL .real-data.shortdata").text(realPNL_pure);
-                                short = true;
+                                $('#realPNL .real-data.shortdata').text(realPNL_pure)
+                                short = true
                             }
-                            if($(this).find('.size-buy').length > 0)
+                            if ($(this).find('.size-buy').length > 0)
                             {
-                                $("#realPNL .real-data.longdata").text(realPNL_pure);
-                                long = true;
+                                $('#realPNL .real-data.longdata').text(realPNL_pure)
+                                long = true
                             }
 
-                            if(long == false)
+                            if (long === false)
                             {
-                                $("#realPNL .real-data.longdata").html('<b class="longside">...<b>');
+                                $('#realPNL .real-data.longdata').html('<b class="longside">...<b>')
                             }
-                            if(short == false)
+                            if (short === false)
                             {
-                                $("#realPNL .real-data.shortdata").html('<b class="shortside">...<b>');
+                                $('#realPNL .real-data.shortdata').html('<b class="shortside">...<b>')
                             }
                         }
-                    });
-                } else {
-                    $(".css-1p1znp9").append(box);
+                    })
+                }
+                else
+                {
+                    $('div[name="orderForm"]').append(box)
                 }
             }
-        }, 1500);
-    } else {
-        console.log('realPNL: ', 'Oops, realPNL doesn\'t work correctly. create an issue on github please.');
+        }, 1500)
+    }
+    else
+    {
+        console.log('realPNL: ', 'Oops, realPNL doesn\'t work correctly. create an issue on github please.')
     }
 });
